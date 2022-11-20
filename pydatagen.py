c02ad37a898df1a9f51ae5424b86df159e1e7a73
ifnesi <inesi@confluent.io>
2022-11-20 19:29:45 +0000

    first commit

---
+++ pydatagen.py
@@ -0,0 +1,505 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2022 Confluent Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Python emulator of the Kafka source connector Datagen
+
+import os
+import sys
+import json
+import time
+import exrex
+import random
+import argparse
+import platform
+import avro.schema
+import commentjson
+
+from confluent_kafka import Producer
+from confluent_kafka.serialization import (
+    SerializationContext,
+    MessageField,
+)
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
+
+
+class AvroParser:
+    def __init__(self, avro_schema_filename: str) -> None:
+        self.payload_iteration_cache = dict()  # In case of arg.properties INTERATION
+        self.keyfield_type = str  # default
+
+        # Read avro schema file (throws exception in case of error)
+        with open(avro_schema_filename, "r") as f:
+            self.avro_schema_original = commentjson.loads(f.read())
+
+        # Validate avro schema (throws exception in case of error)
+        avro.schema.parse(json.dumps(self.avro_schema_original))
+
+        # Cleanup schema
+        self.avro_schema = self._cleanup_schema(self.avro_schema_original)
+
+    def _cleanup_schema(self, schema):
+        """
+        Remove arg.properties from schema (recurring function)
+        """
+        if isinstance(schema, dict):
+            avro_schema = dict()
+            for key, value in schema.items():
+                if key != "arg.properties":
+                    if isinstance(value, list):
+                        avro_schema[key] = list()
+                        for n in value:
+                            avro_schema[key].append(self._cleanup_schema(n))
+                    else:
+                        avro_schema[key] = self._cleanup_schema(value)
+            return avro_schema
+        else:
+            return schema
+
+    def _get_keyfield_type(self, field_type: str):
+        if field_type in ["int", "long"]:
+            return int
+        elif field_type in ["float", "decimal", "bytes", "double"]:
+            return float
+        elif field_type == "boolean":
+            return bool
+        elif field_type == "array":
+            return list
+        else:
+            return str
+
+    def _set_headers(self) -> dict:
+        return {
+            "program": "python",
+            "version": platform.python_version(),
+            "node": platform.node(),
+            "environment": "test",
+        }
+
+    def _set_key(self, message: dict, key_json: bool, keyfield: str):
+        message_key = message[keyfield]
+        if key_json:
+            message_key = json.dumps({keyfield: message_key})
+        else:
+            if self.keyfield_type == bool:
+                message_key = "true" if message_key else "false"
+            elif self.keyfield_type == list:
+                message_key = "[" + ",".join(str(item) for item in message_key) + "]"
+            else:
+                message_key = str(message_key)
+        return message_key
+
+    def generate_payload(
+        self,
+        avro_schema: dict,
+        keyfield: str = None,
+        min_items: int = 1,
+        max_items: int = 1,
+        is_recurring: bool = False,
+    ) -> dict:
+        """
+        Generate random payload as per AVRO schema
+        Args:
+            avro_schema (dict): Avro schema with arg.properties
+            keyfield (str): Key field name
+            min_items (int): Do not set this value, it is used in recurrency cases (array type)
+            max_items (int): Do not set this value, it is used in recurrency cases (array type)
+            is_recurring (bool): Do not set this value, it is used in recurrency cases (array type)
+        Returns:
+            dict: payload
+        """
+
+        # Generated payload
+        payload = dict()
+
+        # Check for arg.properties defined on the 1st level of the schema
+        #   if so randomly chose options (ONLY OPTIONS IS PARSED FOR NOW)
+        args_properties = avro_schema.get("arg.properties")
+        if isinstance(args_properties, dict):
+            if isinstance(args_properties.get("options"), list):
+                payload = random.choice(args_properties.get("options"))
+
+        # Generate payload data/fields types
+        payload_fields = dict()
+        for field in avro_schema.get("fields", list()):
+            if isinstance(field, dict):
+                field_name = field.get("name")
+
+                if field_name is not None:
+                    field_type = field.get("type")
+
+                    if isinstance(field_type, str):
+                        payload_fields[field_name] = {
+                            "type": field_type,
+                            "scale": field.get("scale"),
+                            "arg.properties": field.get("arg.properties"),
+                        }
+
+                    elif isinstance(field_type, dict):
+                        field_type_type = field_type.get("type")
+                        if not is_recurring and keyfield == field_name:
+                            self.keyfield_type = self._get_keyfield_type(
+                                field_type_type
+                            )
+
+                        # arg.properties defined on the field level
+                        args_properties = field.get("arg.properties")
+
+                        # arg.properties defined on the field type level
+                        if args_properties is None:
+                            args_properties = field_type.get("arg.properties")
+
+                        if args_properties is None:
+                            args_properties = dict()
+
+                        # arg.properties defined on the field type items level
+                        if field_type_type == "array":
+                            field_items = field_type.get("items")
+                            if isinstance(field_items, dict):
+                                payload[field_name] = self.generate_payload(
+                                    {"fields": [field_items]},
+                                    min_items=args_properties.get("length", dict()).get(
+                                        "min", 1
+                                    ),
+                                    max_items=args_properties.get("length", dict()).get(
+                                        "max", 1
+                                    ),
+                                    is_recurring=True,
+                                )
+
+                        else:
+                            if isinstance(field_type_type, str):
+                                payload_fields[field_name] = {
+                                    "type": field_type_type,
+                                    "arg.properties": args_properties,
+                                    "scale": field_type.get("scale"),
+                                }
+
+        # Generate random data
+        for field_name, args in payload_fields.items():
+            if payload.get(field_name) is None:
+                payload[field_name] = list()
+                for _ in range(min_items, random.randint(min_items, max_items) + 1):
+                    if isinstance(args.get("arg.properties"), dict) and len(
+                        args.get("arg.properties")
+                    ):
+                        args_properties_type = next(iter(args.get("arg.properties")))
+
+                        # OPTIONS
+                        if args_properties_type == "options":
+                            if isinstance(
+                                args["arg.properties"][args_properties_type], list
+                            ):
+                                payload[field_name].append(
+                                    random.choice(
+                                        args["arg.properties"][args_properties_type]
+                                    )
+                                )
+
+                        # INTERATION
+                        elif args_properties_type == "iteration":
+                            if isinstance(
+                                args["arg.properties"][args_properties_type], dict
+                            ):
+                                iteration_start = args["arg.properties"][
+                                    args_properties_type
+                                ].get("start", 0)
+                                iteration_step = args["arg.properties"][
+                                    args_properties_type
+                                ].get("step", 1)
+                                if self.payload_iteration_cache.get(field_name) is None:
+                                    self.payload_iteration_cache[
+                                        field_name
+                                    ] = iteration_start
+                                else:
+                                    self.payload_iteration_cache[
+                                        field_name
+                                    ] += iteration_step
+                                payload[field_name].append(
+                                    self.payload_iteration_cache[field_name]
+                                )
+
+                        # RANGE
+                        elif args_properties_type == "range":
+                            if isinstance(
+                                args["arg.properties"][args_properties_type], dict
+                            ):
+                                range_min = args["arg.properties"][
+                                    args_properties_type
+                                ].get("min", 0)
+                                range_max = args["arg.properties"][
+                                    args_properties_type
+                                ].get("max", 1)
+                                if isinstance(range_min, int) and isinstance(
+                                    range_max, int
+                                ):
+                                    payload[field_name].append(
+                                        random.randrange(range_min, range_max)
+                                    )
+                                else:
+                                    value = random.uniform(range_min, range_max)
+                                    if isinstance(args.get("scale"), int):
+                                        value = round(value, args.get("scale"))
+                                    payload[field_name].append(value)
+
+                        # REGEX
+                        elif args_properties_type == "regex":
+                            if isinstance(
+                                args["arg.properties"][args_properties_type], str
+                            ):
+                                payload[field_name].append(
+                                    exrex.getone(
+                                        args["arg.properties"][args_properties_type]
+                                    )
+                                )
+
+                if not is_recurring:
+                    if len(payload[field_name]) > 0:
+                        payload[field_name] = payload[field_name][0]
+                    else:
+                        payload.pop(field_name, None)
+
+        return payload
+
+
+def data_dict(
+    data: dict,
+    ctx,
+) -> dict:
+    """
+    Returns a dict representation of a data instance for serialization.
+    Args:
+        data (dict): payload data
+        ctx (SerializationContext): Metadata pertaining to the serialization
+            operation.
+    Returns:
+        dict: Dict populated with user attributes to be serialized.
+    """
+    return dict(data)
+
+
+def deliveryReport(err, msg):
+    """
+    Reports the failure or success of a message delivery.
+    Args:
+        err (KafkaError): The error that occurred on None on success.
+        msg (Message): The message that was produced or failed.
+    Note:
+        In the delivery report callback the Message.key() and Message.value()
+        will be the binary format as encoded by any configured Serializers and
+        not the same object that was passed to produce().
+        If you wish to pass the original object(s) for key and value to delivery
+        report callback we recommend a bound callback or lambda where you pass
+        the objects along.
+    """
+
+    if err is not None:
+        print(f"> ERROR: Delivery failed for Data record {msg.key()}: {err}\n")
+    else:
+        print(
+            f"> Message successfully produced to {msg.topic()}: Partition = {msg.partition()}, Offset = {msg.offset()}\n"
+        )
+
+
+def main(args):
+    path = os.path.dirname(__file__)
+    avro_schema_filename = os.path.join(path, "resources", args.schema_filename)
+    avsc = AvroParser(avro_schema_filename)
+
+    if args.dry_run:
+        print(f"Producing {args.iterations} messages in dry run mode. ^C to exit.\n")
+        try:
+            for msg in range(args.iterations):
+                message = avsc.generate_payload(
+                    avsc.avro_schema_original,
+                    keyfield=args.keyfield,
+                )
+                print(f"message #{msg+1}: {message}")
+
+                # Set headers
+                if args.set_headers:
+                    message_headers = avsc._set_headers()
+                    print(f"headers: {message_headers}")
+
+                # Set key
+                if message.get(args.keyfield):
+                    message_key = avsc._set_key(message, args.key_json, args.keyfield)
+                    print(f"key: {message_key}")
+
+                print()
+
+                time.sleep(args.interval / 1000)
+
+        except KeyboardInterrupt:
+            print("CTRL-C pressed by user")
+
+    else:
+        schema_registry_conf = {
+            "url": args.schema_registry,
+        }
+        schema_registry_client = SchemaRegistryClient(schema_registry_conf)
+
+        producer_conf = {
+            "bootstrap.servers": args.bootstrap_servers,
+        }
+        producer = Producer(producer_conf)
+
+        avro_serializer = AvroSerializer(
+            schema_registry_client,
+            json.dumps(avsc.avro_schema),
+            data_dict,
+        )
+
+        print(
+            f"Producing {args.iterations} messages to topic '{args.topic}'. ^C to exit.\n"
+        )
+        for msg in range(args.iterations):
+            # Serve on_delivery callbacks from previous calls to produce()
+            producer.poll(0.0)
+            try:
+                message = avsc.generate_payload(
+                    avsc.avro_schema_original,
+                    keyfield=args.keyfield,
+                )
+
+                producer_args = {
+                    "topic": args.topic,
+                    "value": avro_serializer(
+                        message,
+                        SerializationContext(
+                            args.topic,
+                            MessageField.VALUE,
+                        ),
+                    ),
+                }
+
+                if not args.silent:
+                    producer_args.update({"on_delivery": deliveryReport})
+                    print(f"message #{msg+1}: {message}")
+
+                # Set headers
+                if args.set_headers:
+                    message_headers = avsc._set_headers()
+                    producer_args.update({"headers": message_headers})
+                    if not args.silent:
+                        print(f"headers: {message_headers}")
+
+                # Set key
+                if message.get(args.keyfield):
+                    message_key = avsc._set_key(message, args.key_json, args.keyfield)
+                    producer_args.update({"key": message_key})
+                    if not args.silent:
+                        print(f"key: {message_key}")
+
+                # Publish message
+                producer.produce(**producer_args)
+
+            except KeyboardInterrupt:
+                print("CTRL-C pressed by user")
+                break
+
+            except ValueError as err:
+                print("> ERROR: Invalid input, discarding record: {err}")
+                continue
+
+            finally:
+                time.sleep(args.interval / 1000)
+
+        print("\nFlushing messages...")
+        producer.flush()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Python emulator of the Kafka source connector Datagen"
+    )
+    parser.add_argument(
+        "--schema-filename",
+        help="Avro schema file name",
+        dest="schema_filename",
+        required=True,
+        type=str,
+    )
+    parser.add_argument(
+        "--keyfield",
+        help="Name of the field to be used as message key",
+        dest="keyfield",
+        type=str,
+        default=None,
+        required="--key-json" in sys.argv,
+    )
+    parser.add_argument(
+        "--key-json",
+        dest="key_json",
+        action="store_true",
+        help="Set key as JSON -> {{keyfield: keyfield_value}}",
+    )
+    parser.add_argument(
+        "--topic",
+        help="Topic name",
+        dest="topic",
+        required="--dry-run" not in sys.argv,
+        type=str,
+    )
+    parser.add_argument(
+        "--set-headers",
+        dest="set_headers",
+        action="store_true",
+        help="Set headers with lineage data",
+    )
+    parser.add_argument(
+        "--dry-run",
+        dest="dry_run",
+        action="store_true",
+        help="Generate and display messages without having them publish",
+    )
+    parser.add_argument(
+        "--bootstrap-servers",
+        dest="bootstrap_servers",
+        default="localhost:9092",
+        help="Bootstrap broker(s) (host[:port])",
+        type=str,
+    )
+    parser.add_argument(
+        "--schema-registry",
+        help="Schema Registry (http(s)://host[:port]",
+        dest="schema_registry",
+        default="http://localhost:8081",
+        type=str,
+    )
+    parser.add_argument(
+        "--iterations",
+        help="Number of messages to be sent",
+        dest="iterations",
+        default=9999999999999,
+        type=int,
+    )
+    parser.add_argument(
+        "--interval",
+        help="Max interval between messages (in milliseconds)",
+        dest="interval",
+        default=500,
+        type=int,
+    )
+    parser.add_argument(
+        "--silent",
+        dest="silent",
+        action="store_true",
+        help="Do not display results on screen (not applicable in dry run mode)",
+    )
+
+    main(parser.parse_args())
---
+++ readme.md
@@ -0,0 +1,143 @@
+# Python emulator of the Kafka source connector Datagen
+
+## Requirements:
+- Python 3.8+
+- see requirements.txt
+
+## Important:
+"resources/" folder forked on 19-Nov-2022 (https://github.com/confluentinc/kafka-connect-datagen/tree/master/src/main/resources)
+
+## Usage and help
+```
+usage: pydatagen.py [-h] --schema-filename SCHEMA_FILENAME [--keyfield KEYFIELD] [--key-json] --topic TOPIC [--set-headers] [--dry-run] [--bootstrap-servers BOOTSTRAP_SERVERS]
+                    [--schema-registry SCHEMA_REGISTRY] [--iterations ITERATIONS] [--interval INTERVAL] [--silent]
+
+options:
+  -h, --help            show this help message and exit
+  --schema-filename SCHEMA_FILENAME
+                        Avro schema file name
+  --keyfield KEYFIELD   Name of the field to be used as message key
+  --key-json            Set key as JSON -> {{keyfield: keyfield_value}}
+  --topic TOPIC         Topic name
+  --set-headers         Set headers with lineage data
+  --dry-run             Generate and display messages without having them publish
+  --bootstrap-servers BOOTSTRAP_SERVERS
+                        Bootstrap broker(s) (host[:port])
+  --schema-registry SCHEMA_REGISTRY
+                        Schema Registry (http(s)://host[:port]
+  --iterations ITERATIONS
+                        Number of messages to be sent
+  --interval INTERVAL   Max interval between messages (in milliseconds)
+  --silent              Do not display results on screen (not applicable in dry run mode)
+```
+
+## Examples:
+### Input (dry run mode)
+```
+python3 pydatagen.py --schema-filename gaming_players.avro --dry-run --set-headers --keyfield player_id --key-json --interval 1000 --iterations 10
+```
+### Output (dry run mode)
+```
+Producing 10 messages in dry run mode. ^C to exit.
+
+message #1: {'player_id': 1072, 'player_name': 'Talbot Cashell', 'ip': '104.16.237.57'}
+headers: {'program': 'python', 'version': '3.10.8', 'node': 'P3W32CDKHC', 'environment': 'test'}
+key: {"player_id": 1072}
+
+message #2: {'player_id': 1053, 'player_name': 'Cirstoforo Joblin', 'ip': '37.136.192.70'}
+headers: {'program': 'python', 'version': '3.10.8', 'node': 'P3W32CDKHC', 'environment': 'test'}
+key: {"player_id": 1053}
+
+message #3: {'player_id': 1047, 'player_name': 'Cort Bainbridge', 'ip': '26.45.199.135'}
+headers: {'program': 'python', 'version': '3.10.8', 'node': 'P3W32CDKHC', 'environment': 'test'}
+key: {"player_id": 1047}
+
+message #4: {'player_id': 1041, 'player_name': 'Fitz Ballin', 'ip': '61.160.45.31'}
+headers: {'program': 'python', 'version': '3.10.8', 'node': 'P3W32CDKHC', 'environment': 'test'}
+key: {"player_id": 1041}
+
+message #5: {'player_id': 1092, 'player_name': 'Faye Beaument', 'ip': '77.208.184.143'}
+headers: {'program': 'python', 'version': '3.10.8', 'node': 'P3W32CDKHC', 'environment': 'test'}
+key: {"player_id": 1092}
+
+message #6: {'player_id': 1034, 'player_name': 'Jori Ottiwill', 'ip': '44.125.117.30'}
+headers: {'program': 'python', 'version': '3.10.8', 'node': 'P3W32CDKHC', 'environment': 'test'}
+key: {"player_id": 1034}
+
+message #7: {'player_id': 1009, 'player_name': 'Winny Cadigan', 'ip': '68.145.84.22'}
+headers: {'program': 'python', 'version': '3.10.8', 'node': 'P3W32CDKHC', 'environment': 'test'}
+key: {"player_id": 1009}
+
+message #8: {'player_id': 1059, 'player_name': 'Riva Rossant', 'ip': '64.39.185.31'}
+headers: {'program': 'python', 'version': '3.10.8', 'node': 'P3W32CDKHC', 'environment': 'test'}
+key: {"player_id": 1059}
+
+message #9: {'player_id': 1051, 'player_name': 'Nathaniel Hallowell', 'ip': '206.228.92.173'}
+headers: {'program': 'python', 'version': '3.10.8', 'node': 'P3W32CDKHC', 'environment': 'test'}
+key: {"player_id": 1051}
+
+message #10: {'player_id': 1095, 'player_name': 'Chryste Wren', 'ip': '141.46.127.99'}
+headers: {'program': 'python', 'version': '3.10.8', 'node': 'P3W32CDKHC', 'environment': 'test'}
+key: {"player_id": 1095}
+```
+
+### Input
+```
+python3 pydatagen.py --schema-filename gaming_players.avro --set-headers --keyfield player_id --key-json --interval 1000 --iterations 10
+```
+### Output
+```
+Producing 10 messages to topic 'test2'. ^C to exit.
+
+message #1: {'player_id': 1079, 'player_name': 'Nertie Zuker', 'ip': '219.151.0.93'}
+headers: {'program': 'python', 'version': '3.10.8', 'node': 'P3W32CDKHC', 'environment': 'test'}
+key: {"player_id": 1079}
+> Message successfully produced to test2: Partition = 0, Offset = 391
+
+message #2: {'player_id': 1020, 'player_name': 'Pattin Eringey', 'ip': '66.106.114.58'}
+headers: {'program': 'python', 'version': '3.10.8', 'node': 'P3W32CDKHC', 'environment': 'test'}
+key: {"player_id": 1020}
+> Message successfully produced to test2: Partition = 0, Offset = 392
+
+message #3: {'player_id': 1006, 'player_name': 'Brenna Woolfall', 'ip': '46.152.206.98'}
+headers: {'program': 'python', 'version': '3.10.8', 'node': 'P3W32CDKHC', 'environment': 'test'}
+key: {"player_id": 1006}
+> Message successfully produced to test2: Partition = 0, Offset = 393
+
+message #4: {'player_id': 1053, 'player_name': 'Cirstoforo Joblin', 'ip': '37.136.192.70'}
+headers: {'program': 'python', 'version': '3.10.8', 'node': 'P3W32CDKHC', 'environment': 'test'}
+key: {"player_id": 1053}
+> Message successfully produced to test2: Partition = 0, Offset = 394
+
+message #5: {'player_id': 1099, 'player_name': 'Raychel Roset', 'ip': '183.237.217.217'}
+headers: {'program': 'python', 'version': '3.10.8', 'node': 'P3W32CDKHC', 'environment': 'test'}
+key: {"player_id": 1099}
+> Message successfully produced to test2: Partition = 0, Offset = 395
+
+message #6: {'player_id': 1062, 'player_name': 'Aryn Haskell', 'ip': '215.235.104.14'}
+headers: {'program': 'python', 'version': '3.10.8', 'node': 'P3W32CDKHC', 'environment': 'test'}
+key: {"player_id": 1062}
+> Message successfully produced to test2: Partition = 0, Offset = 396
+
+message #7: {'player_id': 1006, 'player_name': 'Brenna Woolfall', 'ip': '46.152.206.98'}
+headers: {'program': 'python', 'version': '3.10.8', 'node': 'P3W32CDKHC', 'environment': 'test'}
+key: {"player_id": 1006}
+> Message successfully produced to test2: Partition = 0, Offset = 397
+
+message #8: {'player_id': 1058, 'player_name': 'Aldrich MacVay', 'ip': '198.1.226.227'}
+headers: {'program': 'python', 'version': '3.10.8', 'node': 'P3W32CDKHC', 'environment': 'test'}
+key: {"player_id": 1058}
+> Message successfully produced to test2: Partition = 0, Offset = 398
+
+message #9: {'player_id': 1036, 'player_name': 'Zechariah Wrate', 'ip': '11.107.127.127'}
+headers: {'program': 'python', 'version': '3.10.8', 'node': 'P3W32CDKHC', 'environment': 'test'}
+key: {"player_id": 1036}
+> Message successfully produced to test2: Partition = 0, Offset = 399
+
+message #10: {'player_id': 1100, 'player_name': 'Heindrick Ravenscroft', 'ip': '165.19.12.241'}
+headers: {'program': 'python', 'version': '3.10.8', 'node': 'P3W32CDKHC', 'environment': 'test'}
+key: {"player_id": 1100}
+
+Flushing messages...
+> Message successfully produced to test2: Partition = 0, Offset = 400
+```
