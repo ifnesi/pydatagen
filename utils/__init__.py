1e464ddfdfd95901a82da294566808cc16a08667
ifnesi <inesi@confluent.io>
2024-04-21 16:50:54 +0100

    Minor

--- utils/__init__.py
+++ utils/__init__.py
@@ -8,10 +8,12 @@ class GracefulShutdown:
     This class implements the singleton pattern, the reason is to avoid having the signal handler instantiated multiple times
     """
 
-    def __init__(self, producer):
+    def __init__(self, kafka_producer):
         self.was_signal_set = False
         self.safe_to_terminate = False
-        self.producer = producer
+        self.kafka_producer = kafka_producer
+        self._frame = None
+        self._signal = signal.SIGTERM
         # Set signal handlers
         signal.signal(signal.SIGINT, self.signal_handler)
         signal.signal(signal.SIGTERM, self.signal_handler)
@@ -23,18 +25,28 @@ class GracefulShutdown:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.safe_to_terminate = True
         if self.was_signal_set:
-            self.signal_handler(signal.SIGTERM, None)
+            self.signal_handler(self._signal, self._frame)
 
     def signal_handler(self, sig, frame):
         if not self.was_signal_set:
+            self._signal = sig
+            self._frame = frame
             logging.info("Starting graceful shutdown...")
         if self.safe_to_terminate:
             logging.info("Graceful shutdown completed")
-            if self.producer:
+            if self.kafka_producer:
                 logging.info("Flushing messages...")
                 try:
-                    self.producer.flush()
+                    self.kafka_producer.flush()
                 except Exception as err:
-                    logging.error(err)
+                    logging.error(sys_exc(sys.exc_info()))
             sys.exit(0)
         self.was_signal_set = True
+
+
+def sys_exc(exc_info) -> str:
+    """
+    Get details about Exceptions
+    """
+    exc_type, exc_obj, exc_tb = exc_info
+    return f"{exc_type} | {exc_tb.tb_lineno} | {exc_obj}"
